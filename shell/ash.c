3df19e2b88679396554f9b18eddd7fec72802b07
Ron Yorston <rmy@pobox.com>
2012-02-09 10:10:56 +0000

    Create fake HOME environment variable

--- shell/ash.c
+++ shell/ash.c
@@ -13507,6 +13507,25 @@ init(void)
 			}
 		}
 
+#if ENABLE_PLATFORM_MINGW32
+		p = lookupvar("HOME");
+		if (!p) {
+			const char *hd, *hp;
+
+			hd = lookupvar("HOMEDRIVE");
+			hp = lookupvar("HOMEPATH");
+			if (hd && hp) {
+				char *s;
+
+				if ((s=malloc(strlen(hd) + strlen(hp) + 1)) != NULL) {
+					strcat(strcpy(s, hd), hp);
+					setvar("HOME", s, VEXPORT);
+					free(s);
+				}
+			}
+		}
+#endif
+
 		if (!ENABLE_PLATFORM_MINGW32)
 			setvar("PPID", utoa(getppid()), 0);
 
