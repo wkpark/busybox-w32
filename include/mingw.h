b8f34d0bd385ce8ea2c3540f6a061a75b49f3399
Ron Yorston <rmy@pobox.com>
2012-04-23 10:14:18 +0100

    win32: add popen implementation that uses shell
    Conflicts:
    
    	include/mingw.h

--- include/mingw.h
+++ include/mingw.h
@@ -140,6 +140,11 @@ int mingw_rename(const char*, const char*);
 #define fopen mingw_fopen
 #define rename mingw_rename
 
+FILE *mingw_popen(const char *cmd, const char *mode);
+int mingw_pclose(FILE *fd);
+#define popen mingw_popen
+#define pclose mingw_pclose
+
 /*
  * ANSI emulation wrappers
  */
